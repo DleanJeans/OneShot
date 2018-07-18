432696e8be253b8dc3fc110d1c538ec0286ee8fd
Dlean Jeans <dleanjeans@gmail.com>
2018-07-18 08:11:56 +0700

    Place player at random position

--- godot/source/mechanics/PlayerSpawner.gd
+++ godot/source/mechanics/PlayerSpawner.gd
@@ -4,12 +4,17 @@ var PlayerScene = load("res://source/world/player/Player.tscn")
 
 export(int) var num_players = 99
 export(NodePath) var players
+export(NodePath) var player
 
 onready var _players = get_node(players)
+onready var _player = get_node(player)
 
 func populate():
+	var position = get_parent().get_random_position_in_rect()
+	_player.position = position
+	
 	for i in range(0, num_players):
-		var position = get_parent().get_random_position_in_rect()
+		position = get_parent().get_random_position_in_rect()
 		var player = PlayerScene.instance()
 		
 		player.position = position
